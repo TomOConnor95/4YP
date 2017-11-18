33bf4d520fa8eee70b5346a026a6ce76c623085e
TomOConnor95 <tom5.0@hotmail.com>
2017-11-18 20:50:08 +0000

    More tree tests, coloring nodes that are clicked on and returning values

--- 4YP_Tests/markerClickedCallback.m
+++ 4YP_Tests/markerClickedCallback.m
@@ -1,4 +1,16 @@
-function markerClickedCallback(src,~, index)
-   src.Color = rand(1,3);
-   disp(['Index: ',num2str(index), ' CLICKED']);
+function markerClickedCallback(src,~, index, value)
+
+    disp(['Index: ',num2str(index), ' CLICKED']);
+    disp(['Value: ',num2str(value)]);
+
+%     for i = 1:nnodes(plot_markers_tree)
+%         marker = plot_markers_tree.get(i);
+%         marker.MarkerFaceColor = 'r';
+%         plot_markers_tree = plot_markers_tree.set(i, marker);
+%     end
+
+    src.Color = rand(1,3);
+    src.MarkerFaceColor = [0, 1, 0];
+    %src.MarkerFaceColor = [.8 .6 .6];
+
 end
