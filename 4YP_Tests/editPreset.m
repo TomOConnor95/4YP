a3ecdee364af51d5e064cf1916cea0ee7a4ef7b3
TomOConnor95 <tom5.0@hotmail.com>
2017-11-26 19:11:28 +0000

    bugs with osc sending

--- 4YP_Tests/editPreset.m
+++ 4YP_Tests/editPreset.m
@@ -25,7 +25,7 @@ sendParamsOverOSC(preset{2}, '/freqCoarse', 'ffffff', u);
 preset{3} = [0, 0.01, 0.0, 0.0, 0, 0]; 
 sendParamsOverOSC(preset{3}, '/freqFine', 'ffffff', u);
 %% outputLevels
-preset{4} = [1, 0.5, 0.1, 0.0, 0.0, 0.0]; 
+preset{4} = [1.0, 0.3, 0.1, 0.0, 0.6, 0.0]; 
 sendParamsOverOSC(preset{4}, '/outputLevels', 'ffffff', u);
 %% envAmt
 preset{5} = [0.0, 1.0, 1.0, 1, 1, 1 ]; 
@@ -34,19 +34,19 @@ sendParamsOverOSC(preset{5}, '/envAmt', 'ffffff', u);
 preset{6} = [0.2, 0.5, 0.7, 0.2, 0.6, 0.5]; 
 sendParamsOverOSC(preset{6}, '/lfoADepth', 'ffffff', u);
 %% lfoBDepth
-preset{7} = [0.0, 0.5, 0.3, 0.2, 0.6, 0.5]; 
+preset{7} = [1.0, 0.5, 0.3, 0.9, 0.6, 0.5]; 
 sendParamsOverOSC(preset{7}, '/lfoBDepth', 'ffffff', u);
 %% lfoAParams      %lfo1Rate, lfo1Amp, lfo1PhaseSpread
-preset{8} = [2.10, 0.9, 0.1]; 
+preset{8} = [0.10, 0.9, 0.1]; 
 sendParamsOverOSC(preset{8}, '/lfoAParams', 'fff', u);
 %% lfoBParams      %lfo2Rate, lfo2Amp, lfo2PulseWidth
-preset{9} = [4.10, 0.0, 0.4]; 
+preset{9} = [0.40, 0.5, 0.5]; 
 sendParamsOverOSC(preset{9}, '/lfoBParams', 'fff', u);
 %% envAmpParams  % A, D, S, R, curve
-preset{10} = [0.92, 0.1, 0.9, 0.5]; 
+preset{10} = [0.02, 0.1, 0.9, 0.5]; 
 sendParamsOverOSC(preset{10}, '/envAmpParams', 'ffff', u);
 %% env1Params    %A, D, S, R, curve
-preset{11} = [2.9, 0.9, 1.0, 0.2]; 
+preset{11} = [0.05, 0.9, 1.0, 0.2]; 
 sendParamsOverOSC(preset{11}, '/env1Params', 'ffff', u);
 %% misc         % StereoSpread vibFreqStart/End, %Vib AmtStart/End, VibTime
 preset{12} = [0.7, 5, 1.0, 0.5, 0.4, 4.5]; 
