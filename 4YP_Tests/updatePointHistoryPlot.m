8fdc718ff0a0214558abfbd5087cbe1bd4e16b39
TomOConnor95 <tom5.0@hotmail.com>
2017-11-27 13:40:24 +0000

    Changed plot styling a bit

--- 4YP_Tests/updatePointHistoryPlot.m
+++ 4YP_Tests/updatePointHistoryPlot.m
@@ -10,7 +10,7 @@ P1 = phPlot.sum_tree.get(oldIndex);
 P2 = phPlot.sum_tree.get(newIndex);
 
 
-phPlot.plot_tree = phPlot.plot_tree.addnode(oldIndex, plot([P1(1), P2(1)], [P1(2), P2(2)],'b','LineWidth',3, 'PickableParts','none'));
+phPlot.plot_tree = phPlot.plot_tree.addnode(oldIndex, plot([P1(1), P2(1)], [P1(2), P2(2)], 'Color', [0.4,0.5,0.9,0.5],'LineWidth', 3, 'PickableParts','none'));
 
 phPlot.plot_markers_tree = phPlot.plot_markers_tree.addnode(oldIndex, plot(P2(1), P2(2), 'ro','MarkerSize',12,'MarkerFaceColor',[0.1,1.0,0.1], 'ButtonDownFcn',{@markerClickedCallback2, newIndex}, 'PickableParts','all'));
 
