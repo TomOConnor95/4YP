f6488cce542048c6a5fa71aef0e6efe7a4fc1077
TomOConnor95 <tom5.0@hotmail.com>
2017-12-01 09:46:58 +0000

    Worked on Matlab terminal commands
    Can launch SC by running the SupercolliderSetup.m script

---
+++ 4YP_Tests/SupercolliderSetup.m
@@ -0,0 +1,5 @@
+%% Launch Supercollider script
+!/Applications/SuperCollider.app/Contents/MacOS/sclang /Users/Tom/Documents/4YP_Github/4YP/Supercollider/full_SC_setup.scd&
+
+% Would maybe be better to move SC directory onto MATAL directory so that
+% the files can be referenced more easily
