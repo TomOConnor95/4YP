9638b85570d2875fd59a6df27e8c2f767967a1bb
TomOConnor95 <tom5.0@hotmail.com>
2018-02-09 17:27:35 +0000

    Tidied Up code a bit

--- 4YP_Tests/Single_triangle_interface_test_19_PCASelection_SC.m
+++ 4YP_Tests/Single_triangle_interface_test_19_PCASelection_SC.m
@@ -4,32 +4,8 @@
 
 
 % Miscellaneous Set-up Parameters
-% Mouse clicks
+
 appData = ApplicationDataBlendingInterface();
-% global MOUSE
-% global isMouseClicked
-% MOUSE = [0,0];              % Mouse State
-% isMouseClicked = false;          % Has mouse been pressed?
-
-% 
-% % UI Buttons
-% isSaveButtonPressed = false;
-% isPauseButtonPressed = false;
-% isTimbreButtonPressed = false;
-% isTimeButtonPressed = false;
-% isFreezeSectionToggled = false;
-
-% Preset Markers
-%isMarkerClicked = false;
-
-% appData.markerIndex = 1;
-% appData.currentMarkerIndex = 0;
-% appData.presetsDoubleClicked = [];
-
-% % Program State
-% isBlending = true;          % Is UI in blending mode?
-% isSearching = true;         % Is the program's while loop active
-% isPaused = true;
 
 screenSize = get(0,'Screensize');
 
@@ -37,7 +13,10 @@ screenSize = get(0,'Screensize');
 appData.u = udp('127.0.0.1',57120); 
 fopen(appData.u); 
 
-% Choose initial presets
+%----------------------------------------------------------%
+%----------Choose initial presets--------------------------%
+%----------------------------------------------------------%
+
 
 % Get nameStrings and TypeStrings    Maybe just save these in presetStore
 [~, appData.nameStrings, appData.typeStrings] = createPresetAforOSC();
@@ -55,10 +34,14 @@ fopen(appData.u);
 % presetA = appData.presetStoreVaried(appData.idxSelected(1),:);
 % presetB = appData.presetStoreVaried(appData.idxSelected(2),:);
 % presetC = appData.presetStoreVaried(appData.idxSelected(3),:);
-%                                                
+
 appData.P = presetGeneratorSCMonteCarloMV(presetA, presetB, presetC, appData);
 
 sendAllStructParamsOverOSC(appData.P.presetA, appData.nameStrings, appData.typeStrings, appData.u);
+
+%----------------------------------------------------------%
+%----------------------Figures & Plots---------------------%
+%----------------------------------------------------------%
 % Plot all geometry for Blending Interface
 figure(1)
 appData.G = createBlendingGeometry();
@@ -70,161 +53,17 @@ createPauseButton(appData)
 createSaveButton(appData)
 createFreezeSectionUI(appData)
 
-
 % Create all necessary parameters visualisations
 if appData.displayBarGraphs == true
     figure(2)
     appData.barStruct = createBarGraphsStruct(P.presetA, nameStrings);
     set(gcf,'Position',[-screenSize(3)/26,0,screenSize(3)/2.4,screenSize(4)/2])
 end
-% Create plots to show evolution of parameters and Mouse Points
-% This is done by the presetGenerator class
-
-% Main Loop
 figure(1)
+
 
 % Initialsise the command line printing of parameter
 dispstat('','init')  
-
-% while(isSearching)
-%     
-%     pause(0.01)
-%     % Press button to save final image and quit program
-%     if isSaveButtonPressed
-%         isSaveButtonPressed = false;
-%         
-%         if savePresetsToFile
-%             presetSave = matfile('PresetStoreSC.mat','Writable',true);
-%             presetSave.presetStore(1+length(presetSave.presetStore(:,1)),:) = P.presetA;
-%         end
-%         continue
-%     end
-%     
-%     % Switch presets if a preset marker is clicked
-%     if isMarkerClicked
-%         isMarkerClicked = false;
-%         
-%         P = P.switchPresets(markerIndex);
-%         sendAllStructParamsOverOSC(P.presetA, nameStrings, typeStrings, u);
-%         dispstat(sprintf(preset2string(P.presetA, nameStrings)));
-%         
-%         [P, presetsDoubleClicked] = testForDoubleClicks(P, presetsDoubleClicked, currentMarkerIndex, markerIndex);
-%         
-%         if length(presetsDoubleClicked) >2
-%             
-%             P = setMarkerFaceColours(P, presetsDoubleClicked, [0.8, 0.6, 0.6]);
-%             
-%             P = P.combineSelectedPresets(presetsDoubleClicked);
-%             
-%             presetsDoubleClicked = [];
-%             currentMarkerIndex = 0;
-%             continue
-%             
-%         elseif ~isempty(presetsDoubleClicked)
-%             P = setMarkerFaceColours(P, presetsDoubleClicked, [0,1,0]);
-%         end
-%         
-%         currentMarkerIndex = markerIndex;
-%         
-%         continue 
-%     end
-%     % Press Freeze Time Button
-%     if isTimeButtonPressed
-%         isTimeButtonPressed = false;
-%         
-%         P = P.toggleTimeState();
-%         G = correctlyColourFreezeButtons(G, P);
-%         G = correctlyDisableFreezeToggles(G, P);
-%         continue
-%     end
-%     
-%     % Press Freeze Timbre Button
-%     if isTimbreButtonPressed
-%         isTimbreButtonPressed = false;
-%         
-%         P = P.toggleTimbreState();
-%         G = correctlyColourFreezeButtons(G, P);
-%         G = correctlyDisableFreezeToggles(G, P);
-%         continue
-%     end
-%     
-%     % Press Freeze Section Toggle Button
-%     if isFreezeSectionToggled
-%         isFreezeSectionToggled = false;
-%         
-%         P = P.setFreezeSectionsToggled(checkFreezeToggles(G));
-%         
-%         continue
-%     end
-%     
-%     % Paused State
-%     if isPaused
-%         
-%         if isPauseButtonPressed
-%             isPauseButtonPressed = false;
-%             isMouseClicked = false;
-%             isBlending = true;
-%             isPaused = false;  
-%             
-%             G.but_pause.String = 'Pause On Last Preset';
-%             G.but_pause.BackgroundColor = G.normalButtonColour;
-%             continue
-%         end
-%         	
-%        continue 
-%     end
-%     % Press pause button to pause searching
-%     if isPauseButtonPressed
-%         isPauseButtonPressed = false;
-%         isBlending = false;
-%         isMouseClicked = false;
-%         isPaused = true;
-%         
-%         G.but_pause.String = 'Resume Searching';
-%         G.but_pause.BackgroundColor = G.pauseColour;
-%         
-%         % Revert to last clicked preset
-%         P = P.mixPresets(1,0,0);
-%         sendAllStructParamsOverOSC(P.presetMix, nameStrings, typeStrings, u);
-%         continue
-%     end
-%     
-%     
-%     % If mouse is clicked move to the next generation of presets
-%     if isMouseClicked
-%         isMouseClicked = false;
-%         
-%         P = P.iteratePresets(G.P1);
-%         
-%         if displayBarGraphs
-%             barStruct = updateBarGraphsStruct(P.presetA, barStruct);
-%         end
-%     end
-%     %  Live Preset Blending Step
-%     if isBlending
-%         G.P1 = [MOUSE(1,1);MOUSE(1,2)];
-%         
-%         % has P1 Changed?
-%         if ~isequal(G.P1,G.P1current)
-%             
-%             G = updateGeometry(G);
-%             
-%             [alpha,beta,gamma] = calculatePresetRatios(G);
-%             
-%             P = P.mixPresets(alpha,beta,gamma);
-%             
-%             sendAllStructParamsOverOSC(P.presetMix, nameStrings, typeStrings, u);
-%             
-%             if displayParameters
-%                 dispstat(sprintf(preset2string(P.presetMix, nameStrings)));
-%             end
-%             
-%         end
-%     end
-%     
-%     
-%     
-% end
 
 function mouseMovedBlending (object, eventdata, appData)
     if appData.isPaused == false
@@ -304,8 +143,6 @@ appData.saveButton = uicontrol('style', 'pushbutton',...
 set(appData.saveButton,'HitTest','on')
 
 end
-
-
 
 function saveButtonCallback (object, eventdata, appData)
 % writes continuous mouse position to base workspace
--- 4YP_Tests/markerClickedCallback2.m
+++ 4YP_Tests/markerClickedCallback2.m
@@ -1,44 +1,29 @@
 function markerClickedCallback2(~,~, index, appData)
 
-    disp(['Index: ',num2str(index), ' CLICKED']);
-    appData.markerIndex = index;
+disp(['Index: ',num2str(index), ' CLICKED']);
+appData.markerIndex = index;
 
-%     for i = 1:nnodes(plot_markers_tree)
-%         marker = plot_markers_tree.get(i);
-%         marker.MarkerFaceColor = 'r';
-%         plot_markers_tree = plot_markers_tree.set(i, marker);
-%     end
+appData.P = appData.P.switchPresets(appData.markerIndex);
+sendAllStructParamsOverOSC(appData.P.presetA, appData.nameStrings, appData.typeStrings, appData.u);
+dispstat(sprintf(preset2string(appData.P.presetA, appData.nameStrings)));
 
-    %src.Color = rand(1,3);
-    %src.MarkerFaceColor = [0, 1, 1];
-    %src.MarkerFaceColor = [.8 .6 .6];
+[appData.P, appData.presetsDoubleClicked] = testForDoubleClicks(appData.P, appData.presetsDoubleClicked, appData.currentMarkerIndex, appData.markerIndex);
 
-%     assignin('base','isMarkerClicked',true);
-%     assignin('base','markerIndex',index);
+if length(appData.presetsDoubleClicked) >2
+    
+    appData.P = setMarkerFaceColours(appData.P, appData.presetsDoubleClicked, [0.8, 0.6, 0.6]);
+    
+    appData.P = appData.P.combineSelectedPresets(appData.presetsDoubleClicked);
+    
+    appData.presetsDoubleClicked = [];
+    appData.currentMarkerIndex = 0;
+    
+elseif ~isempty(appData.presetsDoubleClicked)
+    appData.P = setMarkerFaceColours(appData.P, appData.presetsDoubleClicked, [0,1,0]);
+end
 
-    
-    
-   appData.P = appData.P.switchPresets(appData.markerIndex);
-        sendAllStructParamsOverOSC(appData.P.presetA, appData.nameStrings, appData.typeStrings, appData.u);
-        dispstat(sprintf(preset2string(appData.P.presetA, appData.nameStrings)));
-        
-        [appData.P, appData.presetsDoubleClicked] = testForDoubleClicks(appData.P, appData.presetsDoubleClicked, appData.currentMarkerIndex, appData.markerIndex);
-        
-        if length(appData.presetsDoubleClicked) >2
-            
-            appData.P = setMarkerFaceColours(appData.P, appData.presetsDoubleClicked, [0.8, 0.6, 0.6]);
-            
-            appData.P = appData.P.combineSelectedPresets(appData.presetsDoubleClicked);
-            
-            appData.presetsDoubleClicked = [];
-            appData.currentMarkerIndex = 0;
-            
-        elseif ~isempty(appData.presetsDoubleClicked)
-            appData.P = setMarkerFaceColours(appData.P, appData.presetsDoubleClicked, [0,1,0]);
-        end
-        
-        appData.currentMarkerIndex = appData.markerIndex;
-    
-    
-    
+appData.currentMarkerIndex = appData.markerIndex;
+
+
+
 end
