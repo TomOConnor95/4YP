5cb20302de132333b58986f501365ff213417c1f
TomOConnor95 <tom5.0@hotmail.com>
2018-04-23 17:35:40 +0100

    Finished first draft of Preset generation algorithm

--- 4YP_Tests/SigmoidWeightingPlot.m
+++ 4YP_Tests/SigmoidWeightingPlot.m
@@ -7,6 +7,6 @@ plot(ans, 'Color', colours(i*5+1,:), 'LineWidth', 2)
 end
 xticks([1, 2     4     6     8    10    12    14    16    18    20])
 xlim([1,20])
-legend({'CenterRatio = 0.0', 'CenterRatio = 0.2', 'CenterRatio = 0.4', 'CenterRatio = 0.6', 'CenterRatio = 0.8', 'CenterRatio = 1.0'}, 'FontSize', 14)
+legend({'c = 0.0', 'c = 0.2', 'c = 0.4', 'c = 0.6', 'c = 0.8', 'c = 1.0'}, 'FontSize', 14)
 xlabel('\fontsize{14}Preset Number')
 ylabel('\fontsize{14}Weighting')
